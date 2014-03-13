c7d72ca1b96afdc55b4128006eac6cafac0a9d20
penelope <penelope@penelope-tosh>
2014-03-13 12:24:00 +0100

    reformatting and regrouping

--- stcg-admin-menu.php
+++ stcg-admin-menu.php
@@ -7,22 +7,22 @@ require_once 'header.php';
 <h2> Admin Menu - Administrator of STCG only</h2>
 <form>
     <div>  
-        <fieldset>
-        <p><a href="stcg-list-members.php">List of  signed-up members to  (with sorting of columns)</a></p>         
+        <fieldset style = "border: solid black 1px; width: 40%; padding: 20px; padding-top: 20px">
+        <p><a href="stcg-list-all-members.php">List of  signed-up members to date (with sorting of columns)</a></p>         
         <p><a href="stcg-list-event-activities-with-members.php">List of  members at  Events, from  start of STCG</a></p>
 	<p><a href="stcg-input-member-to-find-history.php">View  Event/Activity history of one  member</a></p>
 	</fieldset>
     </div>
     <br /><br />
     <div>
-        <fieldset>
+        <fieldset style = "border: solid black 1px; width: 40%; padding: 20px; padding-top: 20px">
         <p><a href="stcg-input-interest-to-find-members.php">Find all members sharing a particular interest</a></p>
         <p><a href="stcg-input-language-to-find-members.php">Find all  members  speaking a chosen language</a></p>
         </fieldset>
     </div>
     <br /><br />
     <div>
-        <fieldset>
+        <fieldset style = "border: solid black 1px; width: 40%; padding: 20px; padding-top: 20px">
 	<p><a href="stcg-organize-latest-event-activities-members.php">Organize signups at latest Event</a></p>
         </fieldset> 
     </div>
