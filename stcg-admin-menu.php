9ebefd38a0c3012542c46e3dc35f28c283b8eea5
penelope <penelope@penelope-tosh.mshome.net>
2014-07-11 17:08:45 +0200

    tidied up header footer includes

--- stcg-admin-menu.php
+++ stcg-admin-menu.php
@@ -8,8 +8,9 @@ require_once 'header.php';
 <form>
     <div>  
         <fieldset style = "border: solid black 1px; width: 40%; padding: 20px; padding-top: 20px">
-        <p><a href="stcg-list-all-members.php">List of  signed-up members to date (with sorting of columns)</a></p>         
-        <p><a href="stcg-list-event-activities-with-members.php">List of  members at  Events, from  start of STCG</a></p>
+        <p><a href="stcg-list-all-members.php">List of signed-up members to date (with sorting of columns)</a></p> 
+        <p><a href="stcg-list-members-and-details.php">List of members and their details</a></p>      
+        <p><a href="stcg-list-event-activities-with-members.php">List of  members at all Events, from  start of STCG</a></p>
 	<p><a href="stcg-input-member-to-find-history.php">View  Event/Activity history of one  member</a></p>
 	</fieldset>
     </div>
