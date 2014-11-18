5825a7bd5ace9078bd63f76e46375c8436286f36
penelope <penelope@penelope-tosh.mshome.net>
2014-11-18 14:43:33 +0100

    changed dir structure of jqwidgets files

--- header.php
+++ header.php
@@ -14,57 +14,56 @@ session_start();
 <meta name="author" content="Penelope"/>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
 
-<!--=== LINK TAGS ===-->
-<script src="scripts/jquery-ui-1.10.3/jquery-1.10.2.min.js" type="text/javascript" charset="utf-8"></script>
-<script async src="scripts/ajaxrequest.js" type="text/javascript"  charset="utf-8"></script>
-<script async src="scripts/utils.js" type="text/javascript" charset="utf-8"></script>
-
 <link rel="shortcut icon" href="<?php echo DIR_BASE; ?>favicon.ico" /></link>
 
-<link rel="stylesheet" type="text/css" href="widgets/jqwidgets/jqwidgets/styles/jqx.base.css" /></link>
+<!--=== LINK TAGS ===-->
+<script src="scripts/jquery-1.10.2.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="scripts/ajaxrequest.js" type="text/javascript"  charset="utf-8"></script>
+<script src="scripts/utils.js" type="text/javascript" charset="utf-8"></script>
+
+<!-- =========== CSS ======= -->
+<link rel="stylesheet" type="text/css" href="jqwidgets/styles/jqx.base.css" /></link>
 <link rel="stylesheet" type="text/css" href="css/stcg-css.css" /></link>
 <link rel="stylesheet" type="text/css" href="css/theme.css" /></link>
 <link rel="stylesheet" type="text/css" href="css/structure.css" /></link>
 <link rel="stylesheet" type="text/css" href="css/form.css" /></link>
 <link rel="stylesheet" type="text/css" href="css/themes/blue/style.css" /></link>
 
-
-
 <!-- ESSENTIAL JQWIDGETS LIBRARIES -->
-<script async type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqx-all.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxcore.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxdata.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxgrid.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxscrollbar.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxmenu.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxdropdownlist.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxlistbox.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxcore.js"></script>
+<script type="text/javascript" src="jqwidgets/jqx-all.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxdata.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxgrid.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxscrollbar.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxmenu.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxdropdownlist.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxlistbox.js"></script>
 
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxgrid.selection.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxgrid.filter.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxgrid.sort.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxgrid.selection.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxgrid.filter.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxgrid.sort.js"></script>
 
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxgrid.grouping.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxgrid.columnsresize.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxgrid.edit.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxgrid.grouping.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxgrid.columnsresize.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxgrid.edit.js"></script>
 
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxinput.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxnumberinput.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxdatetimeinput.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxcalendar.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxtooltip.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/globalization/globalize.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxinput.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxnumberinput.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxdatetimeinput.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxcalendar.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxtooltip.js"></script>
+<script type="text/javascript" src="jqwidgets/globalization/globalize.js"></script>
 
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxgrid.columnsreorder.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxgrid.pager.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxgrid.columnsreorder.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxgrid.pager.js"></script>
 
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxgrid.export.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxgrid.export.js"></script>
 
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxcheckbox.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxdragdrop.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxbuttons.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxpanel.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxexpander.js"></script>
-<script type="text/javascript" src="widgets/jqwidgets/jqwidgets/jqxdata.export.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxcheckbox.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxdragdrop.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxbuttons.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxpanel.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxexpander.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxdata.export.js"></script>
 
 <!--script type="text/javascript" src="widgets/scripts/gettheme.js"></script-->
